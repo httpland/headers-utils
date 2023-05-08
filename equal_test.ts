b238a70dea4a7c5275594f76239b954f389e67b7
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-08 17:46:31 +0900

    test(equal): add test case

--- equal_test.ts
+++ equal_test.ts
@@ -1,43 +1,48 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 
 import { equalsHeaders } from "./equal.ts";
-import { assertEquals, describe, it } from "./_dev_deps.ts";
+import { assert, assertFalse, describe, it } from "./_dev_deps.ts";
 
 describe("equalsHeaders", () => {
-  it("should pass", () => {
-    const table: [Headers, Headers, boolean][] = [
-      [new Headers({ a: "" }), new Headers(), false],
-      [new Headers(), new Headers({ a: "" }), false],
-      [new Headers({ a: "" }), new Headers({ a: "", b: "" }), false],
-      [new Headers({ a: "", b: "c" }), new Headers({ a: "", d: "c" }), false],
+  it("should return false", () => {
+    const table: [Headers, Headers][] = [
+      [new Headers({ a: "" }), new Headers()],
+      [new Headers(), new Headers({ a: "" })],
+      [new Headers({ a: "" }), new Headers({ a: "", b: "" })],
+      [new Headers({ a: "", b: "c" }), new Headers({ a: "", d: "c" })],
+      [new Headers({ a: "a" }), new Headers({ a: "b" })],
       [
         new Headers({ a: "b", b: "c" }),
         new Headers({ a: "b", b: "c", c: "d" }),
-        false,
       ],
+    ];
 
-      [new Headers(), new Headers(), true],
-      [new Headers({ a: "" }), new Headers({ a: "" }), true],
-      [new Headers([["a", ""]]), new Headers({ a: "" }), true],
+    table.forEach(([a, b]) => {
+      assertFalse(equalsHeaders(a, b));
+    });
+  });
+
+  it("should return true", () => {
+    const table: [Headers, Headers][] = [
+      [new Headers(), new Headers()],
+      [new Headers({ a: "" }), new Headers({ a: "" })],
+      [new Headers([["a", ""]]), new Headers({ a: "" })],
       [
         new Headers({ a: "", b: "c", c: "abc" }),
         new Headers({ a: "", b: "c", c: "abc" }),
-        true,
       ],
       [
         new Headers({ a: " " }),
         new Headers({ a: "" }),
-        true,
       ],
       [
         new Headers({ a: "   a " }),
         new Headers({ a: "        a       " }),
-        true,
       ],
     ];
 
-    table.forEach(([a, b, result]) => {
-      assertEquals(equalsHeaders(a, b), result);
+    table.forEach(([a, b]) => {
+      assert(equalsHeaders(a, b));
     });
   });
 });
